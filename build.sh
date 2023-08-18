6b28778253d7590227dfe39a615eb6a546bfb545
kangjianhong <1071318859@qq.com>
2023-08-18 17:49:46 +0800

    build

--- build.sh
+++ build.sh
@@ -21,9 +21,9 @@ do
 	mkdir -p $dirname
 
 	GOOS=$goos GOARCH=amd64 CGO_ENABLED=0 go build -o $dirname/$filename #-ldflags "-X main.version=tag"
-#	cp -R LICENSE README.md config.toml.sample MANUAL.md CHANGELOG.md changelogs/ $dirname/
+	cp -R LICENSE README.md config.toml.sample MANUAL.md CHANGELOG.md changelogs/ $dirname/
 
-#	zip -r $dirname.zip $dirname
+	zip -r $dirname.zip $dirname
 #	sh  upload-github-release-asset.sh github_api_token=$token owner=solarfs repo=go-chainstorage-cli tag=$tag filename=./$dirname.zip
 #	rm -rf $dirname.zip $dirname
 done
