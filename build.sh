fab1dfa8bed17dfa88296d7318cd8503ce167af0
kangjianhong <1071318859@qq.com>
2023-08-18 17:36:28 +0800

    build

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ set -ex
 
 token="github_api_token"
 
-tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+#tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 
 
 for goos in darwin windows linux
 do
-	dirname="gcscmd_${tag}_${goos}_amd64"
+	dirname="gcscmd_tag_${goos}_amd64"
 	filename="$dirname"
 
 	rm -rf $dirname $dirname.zip
@@ -20,13 +20,11 @@ do
 
 	mkdir -p $dirname
 
-	GOOS=$goos GOARCH=amd64 CGO_ENABLED=0 go build -o $dirname/$filename -ldflags "-X main.version=$tag"
-	cp -R LICENSE README.md config.toml.sample MANUAL.md CHANGELOG.md changelogs/ $dirname/
+	GOOS=$goos GOARCH=amd64 CGO_ENABLED=0 go build -o $dirname/$filename #-ldflags "-X main.version=tag"
+#	cp -R LICENSE README.md config.toml.sample MANUAL.md CHANGELOG.md changelogs/ $dirname/
 
-	zip -r $dirname.zip $dirname
-	sh  upload-github-release-asset.sh github_api_token=$token owner=solarfs repo=go-chainstorage-cli tag=$tag filename=./$dirname.zip
-	rm -rf $dirname.zip $dirname
+#	zip -r $dirname.zip $dirname
+#	sh  upload-github-release-asset.sh github_api_token=$token owner=solarfs repo=go-chainstorage-cli tag=$tag filename=./$dirname.zip
+#	rm -rf $dirname.zip $dirname
 done
-
-#
 
